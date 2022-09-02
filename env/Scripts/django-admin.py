279960e5d29254ece35faad022b0627d4991a2c0
Arargvi1994 <69857824+Arargvi1994@users.noreply.github.com>
2022-09-02 10:54:42 +0100

    Initial

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ARABIAGARRIDO\source\repos\DjangoWebProject4\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
